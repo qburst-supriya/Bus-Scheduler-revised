f39814db92041979fd9ef267e2747dc61480eb56
Supriya <supriyar@qburst.com>
2024-05-21 09:06:38 +0530

    Initial commit

---
+++ Bus Scheduler/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <p className="read-the-docs">
+        Site would be up shortly
+      </p>
+    </>
+  )
+}
+
+export default App
