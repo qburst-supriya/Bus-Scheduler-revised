b3b9be8b10f82fe617bc63dd2bb16999fa1dfc7e
Supriya <supriyar@qburst.com>
2024-05-24 13:57:46 +0530

    Remove unwanted codes

--- Bus Scheduler/src/App.test.tsx
+++ Bus Scheduler/src/App.test.tsx
@@ -3,20 +3,3 @@ import { it, test } from "vitest";
 it("should work", (ctx) => {
   console.log(ctx);
 });
-
-test("should work1", (ctx) => {
-  console.log(ctx);
-});
-
-it("math is easy", ({ expect }) => {
-  expect(2 + 2).toBe(4);
-});
-
-it.concurrent("math is easy", ({ expect }) => {
-  expect(2 + 2).toMatchInlineSnapshot(`4`);
-});
-
-it("math is hard", ({ skip, expect }) => {
-  skip();
-  expect(2 + 2).toBe(5);
-});
